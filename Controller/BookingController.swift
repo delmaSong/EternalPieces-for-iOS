d992727496e6bdffcc100b9dc8e8e8ee55847d1b
dayeong <sdy2856@gmail.com>
2019-10-19 23:27:18 +0900

    오늘 날짜 기반 예약할 수 있는 날짜 선택 가능하도록

--- Controller/BookingController.swift
+++ Controller/BookingController.swift
@@ -58,8 +58,7 @@ class BookingController: UIViewController, UICollectionViewDelegate, UICollectio
     var selectedSize = ""
     
     
-    //모든 예약정보 담을 라벨
-    @IBOutlet var totalBookingInfo: UILabel!
+
     
     
     //도안 아이디값 넘겨받을 변수
@@ -100,7 +99,8 @@ class BookingController: UIViewController, UICollectionViewDelegate, UICollectio
         pickerView(pickSize, didSelectRow: 0, inComponent: 0)
         
         //이번주 월요일 날짜 가져옴
-        getThisMonday()
+        setDate(date:now)
+        
         //도안데이터 세팅
         getDesignData()
         //타티스트 데이터 세팅
@@ -112,19 +112,131 @@ class BookingController: UIViewController, UICollectionViewDelegate, UICollectio
     
 
     
+    //오늘 날짜
+    var stdWeek = Date()
+    let now = Date()
+    
+    //이번주 월요일 기반으로 들어갈 값 계산
+    func setDate(date:Date){
+           //날짜 형식 세팅
+           let dateFormatter = DateFormatter()
+           dateFormatter.dateFormat = "dd"
+
+           //오늘을 기반으로 날짜 값 가져오기
+           var cal = Calendar(identifier: .gregorian)
+           cal.firstWeekday = 2        //주의 시작을 월요일로
+
+           let comps = cal.dateComponents([.yearForWeekOfYear, .weekOfYear], from: date)        //이번주를 가져옴
+           let startOfWeek = cal.date(from: comps)     //이번주의 시작날짜인 월요일의 (연-월-일 시)형식
+           let thisTue = startOfWeek! + 86400          //월요일을 기반으로 그 다음날 값
+           let thisWed = thisTue + 86400               //그 다음날 ..
+           let thisThu = thisWed + 86400
+           let thisFri = thisThu + 86400
+           let thisSat = thisFri + 86400
+           let thisSun = thisSat + 86400
+           
+           
+           //각 요일 버튼에 "dd"형식으로 값 세팅
+           self.mon.setTitle(dateFormatter.string(from: startOfWeek!), for: .normal)
+           self.tue.setTitle(dateFormatter.string(from: thisTue), for: .normal)
+           self.wed.setTitle(dateFormatter.string(from: thisWed), for: .normal)
+           self.thu.setTitle(dateFormatter.string(from: thisThu), for: .normal)
+           self.fri.setTitle(dateFormatter.string(from: thisFri), for: .normal)
+           self.sat.setTitle(dateFormatter.string(from: thisSat), for: .normal)
+           self.sun.setTitle(dateFormatter.string(from: thisSun), for: .normal)
+   
+       
+        //현재 기준 과거일경우 버튼 선택 불가
+        //월요일 버튼
+        if now.timeIntervalSince(startOfWeek!) >= 0 {
+            self.mon.isEnabled = false
+            self.mon.setTitleColor(.gray, for: .normal)
+        }else{
+            self.mon.isEnabled = true
+            self.mon.setTitleColor(.black, for: .normal)
+        }
+        
+        //화요일
+        if now.timeIntervalSince(thisTue) >= 0 {
+            self.tue.isEnabled = false
+            self.tue.setTitleColor(.gray, for: .normal)
+        }else{
+            self.tue.isEnabled = true
+            self.tue.setTitleColor(.black, for: .normal)
+        }
+        //수요일
+        if now.timeIntervalSince(thisTue) >= 0 {
+            self.wed.isEnabled = false
+            self.wed.setTitleColor(.gray, for: .normal)
+        }else{
+            self.wed.isEnabled = true
+            self.wed.setTitleColor(.black, for: .normal)
+        }
+        //목요일
+        if now.timeIntervalSince(thisTue) >= 0 {
+            self.thu.isEnabled = false
+            self.thu.setTitleColor(.gray, for: .normal)
+        }else{
+            self.thu.isEnabled = true
+            self.thu.setTitleColor(.black, for: .normal)
+        }
+        //금요일
+        if now.timeIntervalSince(thisTue) >= 0 {
+            self.fri.isEnabled = false
+            self.fri.setTitleColor(.gray, for: .normal)
+        }else{
+            self.fri.isEnabled = true
+            self.fri.setTitleColor(.black, for: .normal)
+        }
+        //토요일
+        if now.timeIntervalSince(thisTue) >= 0 {
+            self.sat.isEnabled = false
+            self.sat.setTitleColor(.gray, for: .normal)
+        }else{
+            self.sat.isEnabled = true
+            self.sat.setTitleColor(.black, for: .normal)
+        }
+        //일요일
+        if now.timeIntervalSince(thisTue) >= 0 {
+            self.sun.isEnabled = false
+            self.sun.setTitleColor(.gray, for: .normal)
+        }else{
+            self.sun.isEnabled = true
+            self.sun.setTitleColor(.black, for: .normal)
+        }
+
+       }
+       
+    
+    //이전주, 다음주 버튼 이동 확인 플래그
+    var mvFlag: Int = 0
+    
     
     //이전주로 날짜 이동 버튼
     @IBAction func mvBefore(_ sender: Any) {
+        //현재 날짜로부터 이전날짜로는 이동 불가.
+        if mvFlag > 0 {
+            mvFlag-=1
+            self.stdWeek = stdWeek - 604800
+            setDate(date: stdWeek)
+        }
     }
     
     //다음주로 날짜 이동 버튼
     @IBAction func mvAfter(_ sender: Any) {
+        //mvFlag가 4 이하일때만 변동 있음. 현재 날짜로부터 앞으로 4주간만 예약 가능
+        if mvFlag < 5 {
+            mvFlag += 1
+            self.stdWeek = stdWeek + 604800
+            setDate(date: stdWeek)
+        }
     }
     
     
     
     //월요일 선택
     @IBAction func selectMon(_ sender: Any) {
+//        self.selectedDate!.text =
     }
     //화요일 선택
     @IBAction func selectTue(_ sender: Any) {
@@ -160,37 +272,7 @@ class BookingController: UIViewController, UICollectionViewDelegate, UICollectio
     }
     
     
-    //이번주 월요일 기반으로 날짜 세팅
-    func getThisMonday(){
-        //날짜 형식 세팅
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd"
-
-        //오늘을 기반으로 날짜 값 가져오기
-        var cal = Calendar(identifier: .gregorian)
-        cal.firstWeekday = 2        //주의 시작을 월요일로
-        let now = Date()
-        let comps = cal.dateComponents([.yearForWeekOfYear, .weekOfYear], from: now)        //이번주를 가져옴
-        let startOfWeek = cal.date(from: comps)     //이번주의 시작날짜인 월요일의 (연-월-일 시)형식
-        let thisTue = startOfWeek! + 86400          //월요일을 기반으로 그 다음날 값
-        let thisWed = thisTue + 86400               //그 다음날 ..
-        let thisThu = thisWed + 86400
-        let thisFri = thisThu + 86400
-        let thisSat = thisFri + 86400
-        let thisSun = thisSat + 86400
-        
-        
-        //각 요일 버튼에 "dd"형식으로 값 세팅
-        self.mon.setTitle(dateFormatter.string(from: startOfWeek!), for: .normal)
-        self.tue.setTitle(dateFormatter.string(from: thisTue), for: .normal)
-        self.wed.setTitle(dateFormatter.string(from: thisWed), for: .normal)
-        self.thu.setTitle(dateFormatter.string(from: thisThu), for: .normal)
-        self.fri.setTitle(dateFormatter.string(from: thisFri), for: .normal)
-        self.sat.setTitle(dateFormatter.string(from: thisSat), for: .normal)
-        self.sun.setTitle(dateFormatter.string(from: thisSun), for: .normal)
-
-    }
-    
+   
 
       
     
